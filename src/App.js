c8449dbd70bae0311a02e286d07847b2eaaea2a5
Amin Mohammadbagheri <amin@amins-mbp.home>
2024-07-04 00:57:31 -0400

    Adding the mosaic pcn logo

--- src/App.js
+++ src/App.js
@@ -13,7 +13,18 @@ function App({ signOut }) {
   return (
     <View className="App">
       <Card>
-        <Image src={logo} className="App-logo" alt="logo" />
+        {/* <Image src={logo} className="App-logo" alt="logo" /> */}
+        <Image
+          alt="Amplify logo"
+          src="/pcn.jpeg"
+          objectFit="initial"
+          objectPosition="50% 50%"
+          backgroundColor="initial"
+          height="75%"
+          width="75%"
+          opacity="100%"
+          onClick={() => alert('📸 Say cheese!')}
+        />
         <Heading level={1}>We now have Auth!</Heading>
       </Card>
       <Button onClick={signOut}>Sign Out</Button>
