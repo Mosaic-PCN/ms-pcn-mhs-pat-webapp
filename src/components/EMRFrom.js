8d93426cbf2df3aaeecb5d1183d89ad321296304
Amin <amin.mohammadbagheri@gmail.com>
2024-07-18 04:25:52 -0400

    saving

--- src/components/EMRFrom.js
+++ src/components/EMRFrom.js
@@ -30,35 +30,34 @@ const EMRForm = () => {
         <main className="container mt-5">
             <div className="emr-form">
                 <TooltipProvider>
-
                     <div className="emr-column">
-                        <CustomDropdown label={<>Documentation - C&C visit<HelpIcon id="C_C_visit_help" explanation={fieldExplanations['C_C_visit']} /></>} id="C_C_visit" name="C_C_visit" place_holder="Indirect Time" />
-                        <CustomDropdown label={<>Documentation - QI template<HelpIcon id="QI_template_help" explanation={fieldExplanations['QI_template']} /></>} id="QI_template" name="QI_template" place_holder="Indirect Time" />
-                        <CustomDropdown label="Documentation - Menu of Service templates" id="Service_templates" name="Service_templates" place_holder="Indirect Time" />
-                        <CustomDropdown label="Documentation - Clinic Processes" id="inputId" name="inputName" place_holder="Indirect Time" />
-                        <CustomDropdown label="Documentation - Other" id="inputId" name="inputName" place_holder="Indirect Time" />
-                        <CustomDropdown label="Scheduling appointments" id="inputId" name="inputName" place_holder="Indirect Time" />
-                        <DoubleDropdown label="Liaise with external partners" id="inputId" name="inputName" options={options} />
-                        <TripleDropdown label="Provider or clinic inquiry" id="inputId" name="inputName" options={options} max_count={100} />
+                        <CustomDropdown label={<>Documentation - C&C visit<HelpIcon id="C_C_visit_help" explanation={fieldExplanations['CcVisit']} /></>} id="C_C_visit" name="CcVisit" place_holder="Indirect Time" />
+                        <CustomDropdown label={<>Documentation - QI template<HelpIcon id="QI_template_help" explanation={fieldExplanations['QiTemplate']} /></>} id="QI_template" name="QiTemplate" place_holder="Indirect Time" />
+                        <CustomDropdown label={<>Documentation - Menu of Service templates<HelpIcon id="Service_templates_help" explanation={fieldExplanations['ServiceTemplates']} /></>} id="Service_templates" name="ServiceTemplates" place_holder="Indirect Time" />
+                        <CustomDropdown label={<>Documentation - Clinic Processes<HelpIcon id="Documentation_Clinic_Processes_help" explanation={fieldExplanations['DocumentationClinicProcesses']} /></>} id="Documentation_Clinic_Processes" name="DocumentationClinicProcesses" place_holder="Indirect Time" />
+                        <CustomDropdown label={<>Documentation - Other<HelpIcon id="Documentation_Other_help" explanation={fieldExplanations['DocumentationOther']} /></>} id="Documentation_Other" name="DocumentationOther" place_holder="Indirect Time" />
+                        <CustomDropdown label={<>Scheduling appointments<HelpIcon id="Scheduling_appointments_help" explanation={fieldExplanations['SchedulingAppointments']} /></>} id="Scheduling_appointments" name="SchedulingAppointments" place_holder="Indirect Time" />
+                        <DoubleDropdown label={<>Liaise with external partners<HelpIcon id="Liaise_with_external_partners_help" explanation={fieldExplanations['LiaiseWithExternalPartners']} /></>} id="Liaise_with_external_partners" name="LiaiseWithExternalPartners" options={options} />
+                        <TripleDropdown label={<>Provider or clinic inquiry<HelpIcon id="Provider_or_clinic_inquiry_help" explanation={fieldExplanations['ProvideOrClinicInquiry']} /></>} id="Provider_or_clinic_inquiry" name="ProvideOrClinicInquiry" options={options} max_count={100} />
 
                     </div>
                     <div className="emr-column">
-                        <TripleDropdown label="EMR mapping" id="C_C_visit" name="C_C_visit" options={options} />
-                        <TripleDropdown label="EMR searches" id="QI_template" name="QI_template" options={options} />
-                        <TripleDropdown label="EMR data collection" id="Service_templates" name="Service_templates" options={options} />
-                        <TripleDropdown label="EMR data entry" id="inputId" name="inputName" options={options} />
-                        <TripleDropdown label="Meeting - C&C" id="inputId" name="inputName" />
-                        <TripleDropdown label="Meeting - New Member" id="inputId" name="inputName" />
-                        <TripleDropdown label="Meeting - CII/CPAR" id="inputId" name="inputName" />
+                        <TripleDropdown label={<>EMR mapping<HelpIcon id="EMR_mapping_help" explanation={fieldExplanations['EmrMapping']} /></>} id="EMR_mapping" name="EmrMapping" options={options} />
+                        <TripleDropdown label={<>EMR searches<HelpIcon id="EMR_searches_help" explanation={fieldExplanations['EmrSearches']} /></>} id="EMR_searches" name="EmrSearches" options={options} />
+                        <TripleDropdown label={<>EMR data collection<HelpIcon id="EMR_data_collection_help" explanation={fieldExplanations['EmrDataCollection']} /></>} id="EMR_data_collection" name="EmrDataCollection" options={options} />
+                        <TripleDropdown label={<>EMR data entry<HelpIcon id="EMR_data_entry" explanation={fieldExplanations['EmrDataEntry']} /></>} id="EMR_data_entry" name="EmrDataEntry" options={options} />
+                        <TripleDropdown label={<>Meeting - C&C<HelpIcon id="Meeting_C_C_help" explanation={fieldExplanations['MeetingCC']} /></>} id="Meeting_C_C" name="MeetingCC" />
+                        <TripleDropdown label={<>Meeting - New Member<HelpIcon id="Meeting_New_Member_help" explanation={fieldExplanations['MeetingNewMember']} /></>} id="Meeting_New_Member" name="MeetingNewMember" />
+                        <TripleDropdown label={<>Meeting - CII/CPAR<HelpIcon id="Meeting_CII_CPAR_help" explanation={fieldExplanations['MeetingCIICPAR']} /></>} id="Meeting_CII_CPAR" name="MeetingCIICPAR" />
                     </div>
                     <div className="emr-column">
-                        <TripleDropdown label="Meeting - QI" id="inputId" name="inputName" />
-                        <TripleDropdown label="Material dropoff" id="inputId" name="inputName" />
-                        <TripleDropdown label="Clinic Activity - other" id="inputId" name="inputName" />
-                        <TripleDropdown label="Recieving training" id="inputId" name="inputName" />
-                        <TripleDropdown label="Training material creation" id="inputId" name="inputName" />
-                        <TripleDropdown label="Training clinic administrative staff" id="inputId" name="inputName" />
-                        <TripleDropdown label="Training clinic providers (physician, NP, MDT)" id="inputId" name="inputName" />
+                        <TripleDropdown label={<>Meeting - QI<HelpIcon id="C_C_visit_help" explanation={fieldExplanations['MeetingQI']} /></>} id="Meeting_QI" name="MeetingQI" />
+                        <TripleDropdown label={<>Material dropoff<HelpIcon id="C_C_visit_help" explanation={fieldExplanations['MaterialDropoff']} /></>} id="Material_dropoff" name="MaterialDropoff" />
+                        <TripleDropdown label={<>Clinic Activity - other<HelpIcon id="C_C_visit_help" explanation={fieldExplanations['ClinicActivityOther']} /></>} id="Clinic_Activity_other" name="ClinicActivityOther" />
+                        <TripleDropdown label={<>Recieving training<HelpIcon id="C_C_visit_help" explanation={fieldExplanations['RecievingTraining']} /></>} id="Recieving_training" name="RecievingTraining" />
+                        <TripleDropdown label={<>Training material creation<HelpIcon id="C_C_visit_help" explanation={fieldExplanations['TrainingMaterialCreation']} /></>} id="Training_material_creation" name="TrainingMaterialCreation" />
+                        <TripleDropdown label={<>Training clinic administrative staff<HelpIcon id="C_C_visit_help" explanation={fieldExplanations['TrainingClinicAdmins']} /></>} id="Training_clinic_adminins" name="TrainingClinicAdmins" />
+                        <TripleDropdown label={<>Training clinic providers (physician, NP, MDT)<HelpIcon id="C_C_visit_help" explanation={fieldExplanations['TrainingClinicProviders']} /></>} id="Training_clinic_providers" name="TrainingClinicProviders" />
                     </div>
                 </TooltipProvider>
 
